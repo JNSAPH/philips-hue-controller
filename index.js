3ec40cba8ecf917b98d9f187b20ac047d0e61287
JNSAPH <JNSAPH@icloud.com>
2020-10-26 10:57:28 +0100

    Fixed Error Handeling for Color Changing

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ module.exports.changeBrightness = (lamp, bri) => {
 module.exports.changeColor = (lamp, r, g, b) => {
     if (!lamp) throw new Error("Expected a Lamp and a Brightness Value. Check the Docs for more Information.");
     if (!config) throw new Error("Expected a configuration. Check the Docs for more Information");
-    if (r < 255 || !r) throw new Error("Expected a R-Value. R-Value must be between 0 and 255");
-    if (g < 255 || !g) throw new Error("Expected a G-Value. R-Value must be between 0 and 255");
-    if (b < 255 || !b) throw new Error("Expected a B-Value. R-Value must be between 0 and 255");
+    if (r > 255 || r < 0 || !r) throw new Error("Expected a R-Value. R-Value must be between 0 and 255");
+    if (g > 255 || g < 0 || !g) throw new Error("Expected a G-Value. R-Value must be between 0 and 255");
+    if (b > 255 || b < 0 || !b) throw new Error("Expected a B-Value. R-Value must be between 0 and 255");
 
     return axios({
         method: 'put',
